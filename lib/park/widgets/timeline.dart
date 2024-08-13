e01661a010f3ab716376863ca3f7487d3b6558e0
giladomesi <gilad.omesi@mail.huji.ac.il>
2024-08-13 12:24:37 +0300

    maybe fix of current time moving

--- lib/park/widgets/timeline.dart
+++ lib/park/widgets/timeline.dart
@@ -1,3 +1,4 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'package:provider/provider.dart';
@@ -13,30 +14,46 @@ class Timeline extends StatefulWidget {
 }
 
 class _TimelineState extends State<Timeline> {
+  late Timer _timer;
+  DateTime _now = DateTime.now();
+
+  @override
+  void initState() {
+    super.initState();
+    // Update the current time every minute
+    _timer = Timer.periodic(const Duration(seconds: 60), (timer) {
+      setState(() {
+        _now = DateTime.now();
+      });
+    });
+  }
+
+  @override
+  void dispose() {
+    _timer.cancel();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     const double hourWidth = 60;
-    final now = DateTime.now();
 
     // Get the list of arrivals from the provider
-    final arrivals = Provider
-        .of<ArrivalsProvider>(context)
-        .arrivalItems;
+    final arrivals = Provider.of<ArrivalsProvider>(context).arrivalItems;
 
     // Add hours to the timeline
     final List<Widget> hourLines = [];
     for (int i = 0; i < 24; i++) {
-      final String hourLabel = DateFormat('ha').format(
-          DateTime(0, 0, 0, (i + now.hour - 4) % 24));
+      final String hourLabel = DateFormat('ha')
+          .format(DateTime(0, 0, 0, (i + _now.hour - 4) % 24));
       hourLines.add(SizedBox(
-        width: hourWidth - 1,
+        width: hourWidth,
         child: HourLine(hourLabel: hourLabel),
       ));
     }
 
     // Calculate the position of the current time line
-    final double currentHourOffset =
-        (4.5 + now.minute / 60.0) * hourWidth;
+    final double currentHourOffset = (4.5 + _now.minute / 60.0) * hourWidth;
 
     return SingleChildScrollView(
       scrollDirection: Axis.horizontal,
@@ -57,9 +74,9 @@ class _TimelineState extends State<Timeline> {
           ),
           for (final item in arrivals)
             Positioned(
-              left: ((item.startTime.hour - now.hour + 4.5) +
+              left: ((item.startTime.hour - _now.hour + 4.5) +
                   item.startTime.minute / 60.0) *
-                  (hourWidth - 1),
+                  (hourWidth),
               top: 50,
               child: ArrivalIcon(
                 text: item.dog,
