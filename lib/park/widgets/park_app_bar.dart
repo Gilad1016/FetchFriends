384613787311bc890527b345c86a6b09c0ee23e0
giladomesi <gilad.omesi@mail.huji.ac.il>
2024-08-15 11:39:41 +0300

    connect feedback to backend

--- lib/park/widgets/park_app_bar.dart
+++ lib/park/widgets/park_app_bar.dart
@@ -1,7 +1,10 @@
+import 'package:http/http.dart' as http;
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'package:pocketbase/pocketbase.dart';
 import 'package:provider/provider.dart';
-import 'package:feedback/feedback.dart'; // Import the feedback package
+import 'package:feedback/feedback.dart';
+import '../../common/config.dart';
 import '../../common/design/color_pallette.dart';
 import '../../common/providers/router/routes_utils.dart';
 import '../park_provider.dart';
@@ -58,14 +61,32 @@ class _ParkAppBarState extends State<ParkAppBar> {
                 GoRouter.of(context).pushNamed(AppPage.about.toName);
                 break;
               case 2:
-                BetterFeedback.of(context).show((feedback) {
-                  // Handle feedback submission here, e.g., send it to your backend or email
+                BetterFeedback.of(context).show((feedback) async {
+                  final body = <String, dynamic>{
+                    "text": feedback.text,
+                    "extra": feedback.extra,
+                  };
+
+                  try{
+                    await PocketBase(pbAddress).collection('feedback')
+                        .create(body: body,
+                      files: [
+                        http.MultipartFile.fromBytes(
+                          'screenshot',
+                          feedback.screenshot,
+                          filename: 'screenshot.png',
+                        ),
+                      ]);
+                  } catch (e) {
+                    print(e);
+                  }
                 });
                 break;
             }
           },
           icon: const Icon(Icons.more_vert),
-          itemBuilder: (BuildContext context) => <PopupMenuEntry<int>>[
+          itemBuilder: (BuildContext context) =>
+          <PopupMenuEntry<int>>[
             const PopupMenuItem<int>(
               value: 0,
               child: Text('My dogs'),
